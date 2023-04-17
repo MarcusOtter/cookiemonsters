1a1344cec1cf4f0ec51dab2588c91dcc2472a42b
Marcus Otterström <35617441+MarcusOtter@users.noreply.github.com>
2023-04-17 15:42:12 +0200

    Newer chrome version in userAgent

--- src/lib/server/getPage.ts
+++ src/lib/server/getPage.ts
@@ -5,15 +5,15 @@ let browser: Browser;
 
 export async function getDesktopPage(): Promise<Page> {
 	const userAgent =
-		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36";
-	return getPage({ width: 1920, height: 1080 }, userAgent);
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
+	return await getPage({ width: 1920, height: 1080 }, userAgent);
 }
 
 // https://www.browserstack.com/guide/ideal-screen-sizes-for-responsive-design
 export async function getMobilePage(): Promise<Page> {
 	const userAgent =
 		"Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1";
-	return getPage({ width: 360, height: 640 }, userAgent);
+	return await getPage({ width: 360, height: 640 }, userAgent);
 }
 
 async function getPage(viewport: { width: number; height: number }, userAgent: string): Promise<Page> {
