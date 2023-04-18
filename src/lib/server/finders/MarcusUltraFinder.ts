b518bfd1516dd7ef498bc37a60d4069668f64dca
MarcusOtter <github@otterstrom.dev>
2023-04-18 06:27:52 +0200

    Some fixes

--- src/lib/server/finders/MarcusUltraFinder.ts
+++ src/lib/server/finders/MarcusUltraFinder.ts
@@ -11,11 +11,12 @@ export default class MarcusUltraFinder implements BannerFinder {
 		console.log("##### MARCUS ULTRA START #####");
 
 		/*
+			(Idea, not fully implemented and not great yet)
 			- Find all elements with at least one text match (in chilren or itself)
-			- Pick the element with the largest z-index
+			- From the results, pick the element with the largest z-index
 				- If there is a tie, pick the element with a fully opaque background color
-			- Calculate keyword density for this element.
-			- If the element has a higher density than the body, it's likely a banner
+			(skipped) - Calculate keyword density for this element.
+			(skipped) - If the element has a higher density than the body, it's likely a banner
 			- But it could be a wrapper, so we should keep descending down the children until the inner text length changes
 			- If the density changes, we've found the banner (before the change happened)
 		*/
@@ -32,6 +33,10 @@ export default class MarcusUltraFinder implements BannerFinder {
 				const zIndex = window.getComputedStyle(el).zIndex;
 				return zIndex === "auto" ? 0 : parseInt(zIndex);
 			});
+
+			if (!(await element.isIntersectingViewport())) {
+				continue;
+			}
 
 			if (zIndex == highestZIndex) {
 				bestElements.push(element);
@@ -52,6 +57,7 @@ export default class MarcusUltraFinder implements BannerFinder {
 			bestElement = bestElements[0];
 		} else {
 			console.log("THERE WAS A TIE BETWEEN ", bestElements.length, " ELEMENTS");
+			console.log(await Promise.all(bestElements.map(getElementOpeningTag)));
 			for (const element of bestElements) {
 				const isOpaque = await element.evaluate((el) => {
 					const backgroundColor = window.getComputedStyle(el).getPropertyValue("background-color");
@@ -69,35 +75,95 @@ export default class MarcusUltraFinder implements BannerFinder {
 			}
 		}
 
-		// It could be a wrapper, so we should keep descending down the children until the inner text length changes
-		if (bestElement) {
-			let currentElement = bestElement;
-			let currentInnerTextLength = await currentElement.evaluate((el) => el.innerText.length);
-			let nextElement = (await currentElement.$(
+		async function searchForOpaqueElement(
+			element: ElementHandle<HTMLElement>,
+		): Promise<ElementHandle<HTMLElement> | null> {
+			const currentIsOpaque = await element.evaluate((el) => {
+				const backgroundColor = window.getComputedStyle(el).getPropertyValue("background-color");
+				const backgroundImage = window.getComputedStyle(el).getPropertyValue("background-image");
+
+				const colorIsOpaque = !backgroundColor.startsWith("rgba") || backgroundColor.endsWith(", 1)");
+				const hasImage = backgroundImage !== "none";
+				return colorIsOpaque || hasImage;
+			});
+
+			if (currentIsOpaque && (await element.isIntersectingViewport())) {
+				return element;
+			}
+
+			let nextElement = (await element.$(
 				":scope > *:not(style):not(script):not(link):not(meta)",
 			)) as ElementHandle<HTMLElement>;
 
 			while (nextElement) {
-				const nextInnerTextLength = await nextElement.evaluate((el) => el.innerText.length);
-				// This logic is a bit flawed, check blocket.se for example
-				if (nextInnerTextLength !== currentInnerTextLength) {
+				const nextIsOpaque = await nextElement.evaluate((el) => {
+					const backgroundColor = window.getComputedStyle(el).getPropertyValue("background-color");
+					const backgroundImage = window.getComputedStyle(el).getPropertyValue("background-image");
+
+					const colorIsOpaque = !backgroundColor.startsWith("rgba") || backgroundColor.endsWith(", 1)");
+					const hasImage = backgroundImage !== "none";
+					return colorIsOpaque || hasImage;
+				});
+
+				if (nextIsOpaque && (await nextElement.isIntersectingViewport())) {
 					console.log(
-						`Text length between ${await getElementOpeningTag(currentElement)} and ${await getElementOpeningTag(
+						`Found opaque element between ${await getElementOpeningTag(element)} and ${await getElementOpeningTag(
 							nextElement,
-						)} changed from ${currentInnerTextLength} to ${nextInnerTextLength}`,
+						)}`,
 					);
-					break;
+					return nextElement;
 				}
 
-				currentElement = nextElement;
-				currentInnerTextLength = nextInnerTextLength;
-				nextElement = (await currentElement.$(
-					":scope > *:not(style):not(script):not(link):not(meta)",
-				)) as ElementHandle<HTMLElement>;
+				const opaqueChild = await searchForOpaqueElement(nextElement);
+				if (opaqueChild) {
+					return opaqueChild;
+				}
 
-				bestElement = currentElement;
+				nextElement = (
+					await nextElement.$x("following-sibling::*[not(self::style|self::script|self::link|self::meta)]")
+				)[0] as ElementHandle<HTMLElement>;
+			}
+
+			return null;
+		}
+
+		// It could be a wrapper, so we should keep descending down the children until we find an element with an opaque background
+		if (bestElement) {
+			const opaqueElement = await searchForOpaqueElement(bestElement);
+			if (opaqueElement) {
+				bestElement = opaqueElement;
 			}
 		}
+
+		// It could be a wrapper, so we should keep descending down the children until the inner text length changes
+		// if (bestElement) {
+		// 	let currentElement = bestElement;
+		// 	let currentInnerTextLength = await currentElement.evaluate((el) => el.innerText.length);
+		// 	let nextElement = (await currentElement.$(
+		// 		":scope > *:not(style):not(script):not(link):not(meta)",
+		// 	)) as ElementHandle<HTMLElement>;
+
+		// 	while (nextElement) {
+		// 		const nextInnerTextLength = await nextElement.evaluate((el) => el.innerText.length);
+		// 		// This logic is a bit flawed, check blocket.se for example
+		// 		if (nextInnerTextLength !== currentInnerTextLength) {
+		// 			console.log(
+		// 				`Text length between ${await getElementOpeningTag(currentElement)} and ${await getElementOpeningTag(
+		// 					nextElement,
+		// 				)} changed from ${currentInnerTextLength} to ${nextInnerTextLength}`,
+		// 			);
+		// 			break;
+		// 		}
+
+		// 		currentElement = nextElement;
+		// 		currentInnerTextLength = nextInnerTextLength;
+		// 		nextElement = (await currentElement.$(
+		// 			":scope > *:not(style):not(script):not(link):not(meta)",
+		// 		)) as ElementHandle<HTMLElement>;
+
+		// 		bestElement = currentElement;
+		// 	}
+		// }
 
 		if (bestElement) {
 			console.log("The best element is", await getElementOpeningTag(bestElement));
