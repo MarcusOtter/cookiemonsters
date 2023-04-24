5cb07e793bd10c79447532feaa28c260ae9bc6ce
Oliver Palonkorpi <oliver@palonkorpi.fi>
2023-04-24 16:44:34 +0200

    Code can now read the cookie database

--- src/routes/api/analysis/+server.ts
+++ src/routes/api/analysis/+server.ts
@@ -3,6 +3,9 @@ import { getDesktopPage, getMobilePage } from "$lib/server/getPage";
 import AnalysisResult from "$lib/AnalysisResult";
 import getBannerFinders from "$lib/server/finders/getBannerFinders";
 import getErrorMessage from "$lib/getErrorMessage";
+import fs from "fs/promises";
+import path from "path";
+import { parse } from "csv-parse";
 
 export const GET = (async (request) => {
 	let url = request.url.searchParams.get("url");
@@ -23,6 +26,23 @@ export const GET = (async (request) => {
 	}
 }) satisfies RequestHandler;
 
+async function getCookieDatabaseRows(): Promise<string[][]> {
+	const filePath = path.join(process.cwd(), "src/lib/server/open-cookie-database.csv");
+
+	try {
+		const data = await fs.readFile(filePath, "utf-8");
+		parse(data, { columns: false, trim: true }, (err, rows) => {
+			console.log(typeof rows);
+			console.log(rows);
+			return rows;
+		});
+	} catch (error) {
+		console.error("Error reading file:", error);
+	}
+
+	return [];
+}
+
 async function getResults(url: string): Promise<AnalysisResult[]> {
 	const results: AnalysisResult[] = [];
 
@@ -38,6 +58,19 @@ async function getResults(url: string): Promise<AnalysisResult[]> {
 	const requestTimeStart = performance.now();
 	await desktopPage.goto(url, { waitUntil: "networkidle0" });
 	await mobilePage.goto(url, { waitUntil: "networkidle0" });
+
+	const cookies = await desktopPage.cookies();
+
+	console.log("Set cookies:");
+
+	console.log(cookies);
+
+	const cookieDatabaseRows = await getCookieDatabaseRows();
+
+	for (const cookieDbRow of cookieDatabaseRows) {
+		console.log(cookieDbRow);
+	}
+
 	const requestTimeMs = performance.now() - requestTimeStart;
 
 	for (const finder of getBannerFinders()) {
